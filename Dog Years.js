4077f88b73e9a22c9584be50cf7d74e7998e8c0d
Stephen Paakow Aihoon <91861603+aihoon01@users.noreply.github.com>
2021-10-22 21:01:48 +0000

    Add files via upload

---
+++ Dog Years.js
@@ -0,0 +1,15 @@
+const myAge = 22
+//My current age
+let earlyYears = 2
+//earlyYears variable can be changed
+earlyYears *= 10.5
+let laterYears = myAge - 2
+//laterYears is 2 years less than myAge
+laterYears *= 4
+//Converting human years to dog years with a factor of 4.
+console.log(earlyYears)
+console.log(laterYears)
+myAgeInDogYears = earlyYears + laterYears
+myName = 'stephen'.toLowerCase()
+//Converting my name to lowercases.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
