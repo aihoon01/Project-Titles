0f7a25ac5afd4c0fbbf4c73b940bc9d7973c09c7
aihoon1 <stephen.aihoon@amalitech.org>
2021-11-19 16:55:22 +0000

    Mixed Messages Project

---
+++ mixed messages.js
@@ -0,0 +1,10 @@
+const whatsapp= ['hey there am using whatsapp', 'At the movies', 'Urgent calls only'];
+const network = ['MTN everywhere you go', 'Vodafone the future is exciting', 'Glo the fastest the network'];
+const phones = ['Samsung built for tech', 'Apple is premium', 'Infinix is low budget'];
+
+const generateRandomMessage = message => {
+    const randomMessage = Math.floor(Math.random() * message.length);
+    return message[randomMessage];
+}
+console.log(`This is your Whatsapp status: ${generateRandomMessage(whatsapp)} and this is you network status: ${generateRandomMessage(network)}, and this is your phone status: ${generateRandomMessage(phones)}.`);
+
